3112ceff6a41b9a1e6c24d5a40cfda790a1bbe0d
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 20:42:56 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,9 @@
 var grilledCheeseIngredients = [ 'bread', 'mild chees', 'sharp cheese', 'butter', 'tomato', 'garlic'];
+
+var tomatoSauceIngredients = [
+  'tomato',
+  'garlic',
+  'olive oil',
+  'basil',
+  'oregano'
+]
