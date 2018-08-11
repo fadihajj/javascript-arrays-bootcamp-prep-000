6e9976d5712a69bc9bb50975d5a2b17331f949e2
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-11 20:50:25 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray(arr, el){
-  return arr[0]=el;
+  return (arr[0]=el);
 }
 
 function destructiveAddElementToBeginningOfArray(arr, el){
